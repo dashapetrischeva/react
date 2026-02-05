288136595fdf221225468cad62dc0fedb09bfbde
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-02-06 00:41:39 +0100

    Add files via upload
    попітка номер 33

--- lesson_11/src/main.jsx
+++ lesson_11/src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './router/router'
+import '@/index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from '@/router/router'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { store } from '@/store/store'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
