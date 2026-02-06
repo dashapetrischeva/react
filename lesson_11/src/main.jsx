af821ea47592380d072be23c3380b970d154a39b
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-02-06 01:40:59 +0100

    Add files via upload
    попытка номер 35

--- lesson_11/src/main.jsx
+++ lesson_11/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from '@/router/router'
+import { router } from '@/router/Router'
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
 
