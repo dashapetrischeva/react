e8f05fb377c3f2cc77fc05e5cee559b5defe2993
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-06-30 19:54:35 +0200

    Add files via upload
    translate 11

--- lesson_11/src/data/3_data_notebooks.js
+++ lesson_11/src/data/3_data_notebooks.js
@@ -1,14 +1,14 @@
 export const notebooksList = [
   {
     id: 1,
-    title: 'Ноутбук ASUS TUF Gaming A15 (2023)',
+    title: 'Laptop ASUS TUF Gaming A15 (2023)',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/349586929.jpg',
     price: 51999,
   },
   {
     id: 2,
-    title: 'Ноутбук ASUS TUF Gaming F15 FX506HF-HN038 ...',
+    title: 'Laptop ASUS TUF Gaming F15 FX506HF-HN038 ...',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/323437245.jpg',
 
@@ -16,28 +16,28 @@ export const notebooksList = [
   },
   {
     id: 3,
-    title: 'Ноутбук HP Victus Gaming Laptop ...',
+    title: 'Laptop HP Victus Gaming Laptop ...',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/368225648.jpg',
     price: 34999,
   },
   {
     id: 4,
-    title: 'Ноутбук Lenovo Legion 5 Pro 16ACH6H',
+    title: 'Laptop Lenovo Legion 5 Pro 16ACH6H',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/323437245.jpg',
     price: 45999,
   },
   {
     id: 5,
-    title: 'Ноутбук Acer Nitro 5 AN515-58',
+    title: 'Laptop Acer Nitro 5 AN515-58',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/368225648.jpg',
     price: 37999,
   },
   {
     id: 6,
-    title: 'Ноутбук MSI Katana GF66 12UC',
+    title: 'Laptop MSI Katana GF66 12UC',
     imgSrc:
       'https://content.rozetka.com.ua/goods/images/original/349586929.jpg',
     price: 39999,
