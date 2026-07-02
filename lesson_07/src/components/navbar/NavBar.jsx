fd9bfa1b4da996f22f6e6397fce5403e72c59981
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-07-02 22:13:17 +0200

    Add files via upload
    navbar translate

--- lesson_07/src/components/navbar/NavBar.jsx
+++ lesson_07/src/components/navbar/NavBar.jsx
@@ -1,56 +1,24 @@
 import styles from './NavBar.module.css'
 import { NavLink } from 'react-router'
-
 function NavBar() {
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li>
-					<NavLink
-						to="/"
-						className={({ isActive }) =>
-							[styles['nav-link'], isActive ? styles.active : ''].join(' ')
-						}
-					>
-						Home
-					</NavLink>
+					<NavLink to='/' className={({ isActive }) => [styles['nav-link'], isActive ? styles.active : ''].join(' ')}>Головна</NavLink>
 				</li>
-
 				<li>
-					<NavLink
-						to="/shop"
-						className={({ isActive }) =>
-							[styles['nav-link'], isActive ? styles.active : ''].join(' ')
-						}
-					>
-						Shop
-					</NavLink>
+					<NavLink to='/shop' className={({ isActive }) => [styles['nav-link'], isActive ? styles.active : ''].join(' ')}>Магазин</NavLink>
 				</li>
-
 				<li>
-					<NavLink
-						to="/payment"
-						className={({ isActive }) =>
-							[styles['nav-link'], isActive ? styles.active : ''].join(' ')
-						}
-					>
-						Payment Rules
-					</NavLink>
+					<NavLink to='/payment' className={({ isActive }) => [styles['nav-link'], isActive ? styles.active : ''].join(' ')}>Правила оплати</NavLink>
 				</li>
-
 				<li>
-					<NavLink
-						to="/contacts"
-						className={({ isActive }) =>
-							[styles['nav-link'], isActive ? styles.active : ''].join(' ')
-						}
-					>
-						Contacts
-					</NavLink>
+					<NavLink to='/contacts' className={({ isActive }) => [styles['nav-link'], isActive ? styles.active : ''].join(' ')}>Контакти</NavLink>
 				</li>
 			</ul>
 		</nav>
-	)
+	);
 }
 
-export default NavBar
+export default NavBar;
